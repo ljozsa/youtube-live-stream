9a7e75dd5ac71b2eb68057d507442353d49c944f
Ladislav Jozsa <ljozsa@redhat.com>
2017-01-26 18:51:22 +0100

    update youtube keys

--- streamer.py
+++ streamer.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 import libtmux
 
-e105 = ('e105','226.22.36.130:4464','redhatczech-6420.z4c1-2mu7-xves-fjx8')
-a112 = ('a112','226.22.36.134:4472','redhatczech-6420.ppeu-y6m0-db0s-0br7')
-a113 = ('a113','226.22.36.136:4476','redhatczech-6420.9ugj-rq34-96k0-6kjj')
+e104 = ('e104','226.22.36.128:4460','4p3s-bz2a-c9dh-6ws9')
+e105 = ('e105','226.22.36.130:4464','rxb4-xtjr-3wwx-0gtq')
+a112 = ('a112','226.22.36.134:4472','0k3z-e1zj-jryz-8s9s')
+a113 = ('a113','226.22.36.136:4476','fxys-sb7h-pqhu-apg1')
 d105_roomcam = ('d105_roomcam','226.22.36.120:4444','redhatczech-6420.abwy-9fs2-9sgk-8wuw')
-d105 = ('d105','226.22.36.121:4446','redhatczech-6420.yhah-f4pt-y2rj-4uc2')
-d0207 = ('d0207','226.22.36.125:4454','redhatczech-6420.yjbx-wtkq-p5vz-exfm')
-d0206 = ('d0206','226.22.36.123:4450','redhatczech-6420.6975-9g70-13e5-6vww')
-e112 = ('e112','226.22.36.127:4458','redhatczech-6420.q3xu-jpus-a8mg-55jx')
-e104 = ('e104','226.22.36.128:4460','redhatczech-6420.zsta-xckx-vyj2-6vkw')
+d105 = ('d105','226.22.36.121:4446','f805-1zek-5pws-cdxw')
+d0207 = ('d0207','226.22.36.125:4454','1h7g-axbq-peqp-7g9p')
+d0206 = ('d0206','226.22.36.123:4450','1xub-purk-m9uk-2rtu')
+e112 = ('e112','226.22.36.127:4458','9up5-w9bc-wchb-cr28')
 
 rooms_to_stream = ( e105, a112, a113, d105_roomcam, d105, d0207, d0206, e112, e104 )
 windows = []
